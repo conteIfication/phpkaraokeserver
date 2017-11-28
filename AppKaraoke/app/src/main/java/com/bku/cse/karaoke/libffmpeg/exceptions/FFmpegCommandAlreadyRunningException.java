a7cea9a38c1eefcb1530910def918e35b042e666
Trung Thong <trung_thong95@yahoo.com>
2017-11-28 19:38:10 +0700

    +change all

--- AppKaraoke/app/src/main/java/com/bku/cse/karaoke/libffmpeg/exceptions/FFmpegCommandAlreadyRunningException.java
+++
@@ -1,9 +0,0 @@
-package com.bku.cse.karaoke.libffmpeg.exceptions;
-
-public class FFmpegCommandAlreadyRunningException extends Exception {
-
-    public FFmpegCommandAlreadyRunningException(String message) {
-        super(message);
-    }
-
-}
--- AppKaraoke/app/src/main/java/com/bku/cse/karaoke/libffmpeg/exceptions/FFmpegNotSupportedException.java
+++
@@ -1,9 +0,0 @@
-package com.bku.cse.karaoke.libffmpeg.exceptions;
-
-public class FFmpegNotSupportedException extends Exception {
-
-    public FFmpegNotSupportedException(String message) {
-        super(message);
-    }
-
-}
